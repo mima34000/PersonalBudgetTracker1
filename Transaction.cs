c018ce83142e4e73113fc8f84722965340127919
Mirjana Ristic <mima34.mg@gmail.com>
2026-02-12 19:51:07 +0100

    Add Transaction class

---
+++ Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// This class represents one transaction (income or expense)
+public class Transaction
+{
+    // Description of the transaction, for example "Salary" or "Groceries"
+    public string Description { get; set; }
+
+    // Amount of money
+    // Positive value = income
+    // Negative value = expense
+    public decimal Amount { get; set; }
+
+    // Category of the transaction, for example "Food", "Transport", "Income"
+    public string Category { get; set; }
+
+    // Date of the transaction written as text (YYYY-MM-DD)
+    // The assignment requires the date to be stored as a string
+    public string Date { get; set; }
+
+    // This method prints all information about the transaction to the console
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{Date} | {Category} | {Description} | {Amount}");
+    }
+}
